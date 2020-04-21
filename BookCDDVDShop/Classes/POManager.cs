5f668bb77d305d793f727108b0f88d7242b68df3
Andrew Larkins <alarkins93@yahoo.com>
2020-04-21 01:18:08 -0400

    did the DB class

--- BookCDDVDShop/Classes/POManager.cs
+++ BookCDDVDShop/Classes/POManager.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+ * Eric Friedman
+ * Andrew Larkins
+ * CIS-3309
+ * Final Project
+ */
+
 namespace BookCDDVDShop.Classes
 {
     class POManager
